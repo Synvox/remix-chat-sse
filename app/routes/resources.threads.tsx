8611fe0cc66bb03fee2c2eb6191ab7710cac3afe
Ryan Allred <ryanallredxyz@gmail.com>
2023-09-14 17:18:59 -0600

    put prettier back

--- app/routes/resources.threads.tsx
+++ app/routes/resources.threads.tsx
@@ -1,2 +1,4 @@
+// This route is only necessary because you can't
+// use a fetcher with a pathless layout route.
 import { loader, shouldRevalidate } from "./_app";
 export { loader, shouldRevalidate };
